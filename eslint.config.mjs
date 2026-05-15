47fffbbc0ab72ad9a9dfe827f88e119b2098cd20
Keeththigan <keeththi2003@gmail.com>
2026-05-15 13:50:29 +0530

    chore: update ESLint configuration to enforce stricter TypeScript rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,22 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    rules: {
+     // 1. No "any" type - forces you to define interfaces
+      "@typescript-eslint/no-explicit-any": "error",
+      
+      // 2. No unused variables - keeps the code clean
+      "no-unused-vars": "off", // Turned off to use the TS version below
+      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
+      
+      // 3. Force "const" instead of "let" where possible
+      "prefer-const": "error",
+    },
+
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
