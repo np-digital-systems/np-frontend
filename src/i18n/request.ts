9a0d217ace396c8abe2f2bae34d85f4464250075
Keeththigan <keeththi2003@gmail.com>
2026-06-16 12:37:36 +0530

    feat: implement i18n support with next-intl and language middleware

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,16 +1,13 @@
 import { getRequestConfig } from 'next-intl/server';
 import { notFound } from 'next/navigation';
 
-// 1. Define your locales array
 export const locales = [ 'ta','en'] as const;
 
-// 2. Extract the 'Locale' type explicitly from the array (fixes the error)
 export type Locale = (typeof locales)[number]; 
 
 export default getRequestConfig(async ({ requestLocale }) => {
   const locale = await requestLocale;
 
-  // 3. Cast the check using the local type
   if (!locale || !locales.includes(locale as Locale)) {
     notFound();
   }
